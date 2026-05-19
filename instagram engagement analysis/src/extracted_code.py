78936d42e52ff1c6ae29f98db218508666ad4049
Bharathnivasan J <cb.ps.p2asd23005@cb.students.amrita.edu>
2026-05-19 06:48:22 +0530

    Add files via upload

---
+++ instagram engagement analysis/src/extracted_code.py
@@ -0,0 +1,18 @@
+read_csv
+Requirement already satisfied: matplotlib in c:\users\91733\anaconda3\lib\site-packages (from wordcloud) (3.7.2)
+Requirement already satisfied: contourpy>=1.0.1 in c:\users\91733\anaconda3\lib\site-packages (from matplotlib->wordcloud) (1.0.5)
+Requirement already satisfied: cycler>=0.10 in c:\users\91733\anaconda3\lib\site-packages (from matplotlib->wordcloud) (0.11.0)
+Requirement already satisfied: fonttools>=4.22.0 in c:\users\91733\anaconda3\lib\site-packages (from matplotlib->wordcloud) (4.25.0)
+Requirement already satisfied: kiwisolver>=1.0.1 in c:\users\91733\anaconda3\lib\site-packages (from matplotlib->wordcloud) (1.4.4)
+Requirement already satisfied: packaging>=20.0 in c:\users\91733\anaconda3\lib\site-packages (from matplotlib->wordcloud) (23.1)
+Requirement already satisfied: pyparsing<3.1,>=2.3.1 in c:\users\91733\anaconda3\lib\site-packages (from matplotlib->wordcloud) (3.0.9)
+Requirement already satisfied: python-dateutil>=2.7 in c:\users\91733\anaconda3\lib\site-packages (from matplotlib->wordcloud) (2.8.2)
+Requirement already satisfied: six>=1.5 in c:\users\91733\anaconda3\lib\site-packages (from python-dateutil>=2.7->matplotlib->wordcloud) (1.16.0)
+matplotlib.pyplot
+similar flexibility) or `histplot` (an axes-level function for histograms).
+sns.distplot(data['Reach'])
+similar flexibility) or `histplot` (an axes-level function for histograms).
+sns.distplot(data['From Hashtags'])
+similar flexibility) or `histplot` (an axes-level function for histograms).
+sns.distplot(data['Likes'])
+matplotlib
